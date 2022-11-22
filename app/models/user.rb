ebe35ad5e34ce53a99cba31ad93296ad17ae0a78
Anas Abubakar <anasabubakar2015@gmail.com>
2022-11-22 15:13:29 +0100

    Create models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :deals
+  has_many :categories
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :email, :password, presence: true
 end
